7f0064a261584d876bb6b24c3eec9f78fa7f48c7
Muhammad Wael <muhammad.wael2040@gmail.com>
2024-09-14 06:25:52 +0300

    adding important modules

---
+++ main.c
@@ -0,0 +1,30 @@
+
+#include <stdint.h>
+#include <stm32f103x6.h>
+#include "Stm32_F103C6_GPIO.h"
+#include "lcd.h"
+
+
+void clock_init()
+{
+	//Enable port A clock
+	RCC_GPIOA_CLK_EN();
+	//Enable port B clock
+	RCC_GPIOB_CLK_EN();
+}
+void wait_ms(uint32_t time){
+	uint32_t i,j;
+	for(i=0;i<time;i++)
+		for(j=0;j<255;j++);
+}//hint time :100 == 1 sec
+
+int main(void)
+{
+	clock_init();
+	lcd_init();
+	while(1)
+	{
+		// implementation
+	}
+
+}
